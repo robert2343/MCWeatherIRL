2b3644d53ebe5c61f881d26d1122b440281090b1
Robert Ketler <rketler@gmail.com>
2023-01-21 21:20:04 -0500

    arduino serial

---
+++ CommTest/src/Main.java
@@ -0,0 +1,20 @@
+import com.fazecast.jSerialComm.SerialPort;
+
+public class Main {
+    public static void main(String[] args) {
+        SerialPort comPort = SerialPort.getCommPorts()[0];
+        comPort.openPort();
+        try {
+            while (true)
+            {
+                while (comPort.bytesAvailable() == 0)
+                    Thread.sleep(20);
+
+                byte[] readBuffer = new byte[comPort.bytesAvailable()];
+                int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+                System.out.println("Read " + numRead + " bytes.");
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+        comPort.closePort();
+    }
+}
