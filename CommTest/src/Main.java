f3b44fb04fe61a2db4e3f34c6667762c57c93bea
Robert Ketler <rketler@gmail.com>
2023-01-21 22:04:59 -0500

    successfully reading com3 in java

--- CommTest/src/Main.java
+++ CommTest/src/Main.java
@@ -1,18 +1,31 @@
 import com.fazecast.jSerialComm.SerialPort;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //https://github.com/Fazecast/jSerialComm/wiki/Nonblocking-Reading-Usage-Example
         SerialPort comPort = SerialPort.getCommPorts()[0];
         comPort.openPort();
         try {
+            String currentString = "";
             while (true)
             {
                 while (comPort.bytesAvailable() == 0)
                     Thread.sleep(20);
 
                 byte[] readBuffer = new byte[comPort.bytesAvailable()];
-                int numRead = comPort.readBytes(readBuffer, readBuffer.length);
-                System.out.println("Read " + numRead + " bytes.");
+                comPort.readBytes(readBuffer, readBuffer.length);
+                String str = new String(readBuffer);
+                String leftPart = "";
+                String rightPart = "";
+                if(str.indexOf("\n") == -1)
+                {
+                    currentString += str;
+                }
+                else {
+                    leftPart = str.substring(0, str.indexOf("\n"));
+                    rightPart = str.substring(str.indexOf("\n"));
+                    System.out.print(currentString + leftPart);
+                    currentString = rightPart;
+                }
             }
         } catch (Exception e) { e.printStackTrace(); }
         comPort.closePort();
